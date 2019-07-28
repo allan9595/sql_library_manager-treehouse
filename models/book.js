74f19069613889219359794f06ccf73d43b83b38
*allan9595* <bohanzhang@my.unt.edu>
2019-07-28 19:56:37 -0400

    finish basic CURD

--- models/book.js
+++ models/book.js
@@ -1,4 +1,5 @@
 'use strict';
+const dateFormat = require('dateformat');
 module.exports = (sequelize, DataTypes) => {
   const Book = sequelize.define('Book', {
     title: {
@@ -16,7 +17,16 @@ module.exports = (sequelize, DataTypes) => {
     genre: DataTypes.STRING,
     year: DataTypes.INTEGER,
     
-  }, {});
+  }, {
+    instanceMethods: {
+      publishedAt: function() {
+        return dateFormat(this.createdAt, "dddd, mmmm dS, yyyy, h:MM TT");
+      },
+      shortDescription: function(){ 
+        return this.body.length > 30 ? this.body.substr(0, 30) + "..." : this.body;
+      }
+    }
+  });
   Book.associate = function(models) {
     // associations can be defined here
   };
--- routes/index.js
+++ routes/index.js
@@ -9,35 +9,55 @@ router.get('/', (req, res, next) => {
 /* GET book page. */
 router.get('/books', (req, res, next) => {
   Book.findAll({order: [["createdAt", "DESC"]]}).then((books) => {
-    res.render('index', { books, title:'Book' });
+    res.render('index', { books, title:'Books' });
   })
   
 });
 
 /* GET book new */
 router.get('/books/new', (req, res, next) => {
-  
+  res.render('new-book', {book: Book.build()})
 });
 
 /* POST book new */
 router.post('/books/new', (req, res, next) => {
-  
+  Book.create({
+    title: req.body.title, 
+    author: req.body.author,
+    genre: req.body.genre,
+    year: req.body.year
+  }).then(() => {
+    res.redirect('/books')
+  })
 });
 
 
 /* GET book id */
 router.get('/books/:id', (req, res, next) => {
-  
+  Book.findByPk(req.params.id).then((book) => {
+    res.render('update-book',{book})
+  })
 });
 
 
 /* post book id */
 router.post('/books/:id', (req, res, next) => {
-  
+  Book.update({
+    title: req.body.title, 
+    author: req.body.author,
+    genre: req.body.genre,
+    year: req.body.year
+  }, {
+    where: {id: req.params.id}
+  }).then(() => {
+    res.redirect('/books')
+  })
 });
 
 /* post book id delete*/
 router.post('/books/:id/delete', (req, res, next) => {
-  
+  Book.destroy({where: {id: req.params.id}}).then(() => {
+    res.redirect('/books')
+  })
 });
 module.exports = router;
